e4f107b276ee3293dc77b374696700b302cb0c5e
AminEl93 <amin_11_iverson@hotmail.com>
2023-10-04 19:35:11 +0200

    Importación de las pipes personalizadas y su ruta

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -7,13 +7,24 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { OrderComponent } from './pages/order/order.component';
+
+import { CanFlyPipe } from './custom-pipes/can-fly.pipe';
+import { ToggleCasePipe } from './custom-pipes/toggle-case.pipe';
+import { SortByPipe } from './custom-pipes/sort-by.pipe';
 
 @NgModule({
     declarations: [
-    BasicsPageComponent,
-    NumbersPageComponent,
-    UncommonPageComponent
-  ],
+        BasicsPageComponent,
+        NumbersPageComponent,
+        UncommonPageComponent,
+        OrderComponent,
+
+        // Custom Pipes
+        CanFlyPipe,
+        ToggleCasePipe,
+        SortByPipe    
+    ],
     imports: [
         CommonModule,
         PipesRoutingModule,
